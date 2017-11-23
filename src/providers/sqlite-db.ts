87d1a18a8d53dac709e5db162548f5775f2936db
Gurjeet <contact@novelucent.com>
2017-11-23 10:30:01 +0530

    Restructured to fix Promise hierarchy

--- src/providers/sqlite-db.ts
+++ src/providers/sqlite-db.ts
@@ -5,7 +5,6 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class SqliteDbProvider {
-  public debug = true;
   private options:{name:string, location:string,createFromLocation:number} = {
     name: "data.db",
     location: 'default',
@@ -15,20 +14,9 @@ export class SqliteDbProvider {
   constructor(private platform: Platform, private sqlite: SQLite) { }
 
   private loadDB(){
-    return new Promise((resolve, reject) => {
-      this.platform.ready().then(() => {
-        //(window as any).plugins.sqlDB.remove(this.options.name, 0);
-        this.sqlite.create(this.options)
-          .then((db: SQLiteObject) => {
-            if(this.debug){
-              console.log('DB opened successfully');
-            }
-            resolve(db);
-          })
-          .catch(e => {
-            reject(e)
-          });
-      });
+    return this.platform.ready().then(() => {
+      //(window as any).plugins.sqlDB.remove(this.options.name, 0);
+      return this.sqlite.create(this.options);
     });
   }
 
@@ -45,28 +33,16 @@ export class SqliteDbProvider {
   }
 
   public query(query, params: any[] = []){
-    if(this.debug){
-      console.log('Got query request');
-    }
     return this.loadDB()
       .then((db: SQLiteObject) => {
-        if(this.debug){
-          console.log('Got DB connection. Running query');
-        }
-        return db.executeSql(query, params)
-          .then((data) => {
-            if(this.debug){
-              console.log(JSON.stringify(data));
-            }
-            return data;
-          })
-          .catch(e => {
-            console.log('Query error:'+JSON.stringify(e));
-            return [];
-          });
+        return db.executeSql(query, params);
+      })
+      .then((data) => {
+        console.log(JSON.stringify(data));
+        return data;
       })
       .catch(e => {
-        console.log('DB connection error:'+JSON.stringify(e));
+        console.log('Database Error: '+JSON.stringify(e));
         return [];
       });
   }
